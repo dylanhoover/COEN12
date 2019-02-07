4a37a1f5e30233e0253d57ef8cffb16bddcc5d99
dylanhoover <dylangranthoover@gmail.com>
2019-02-07 12:33:49 -0800

    Project 2 and 3 added added text files used for testing in etc directory

--- src/project2/sorted.c
+++ src/project2/sorted.c
@@ -11,12 +11,6 @@ struct set
     int length;
     char* *data;
 };
-/*
- *Function: createSet
- *
- *Description: creates the set that holds the arrays
- *
- */
 
 SET *createSet(int maxElts)
 {
@@ -30,44 +24,32 @@ SET *createSet(int maxElts)
     assert(sp->data != NULL);
     return(sp);
 }
-/*
- *Function: search
- *
- *Description: searches for the element using binary search method
- *	O(logn)
- *
- */
+
 int search(SET *sp, char *elt, bool *found)
 {
     int lo, hi, mid;
     lo = 0;
     hi = (sp->count -1);
+    *found = true;
     int compare;
     while(lo<=hi)
     {
         mid = (lo+hi)/2; //set mid
         compare = strcmp(sp->data[mid], elt); //set the compare variable up
-        if(compare == 0) //when compare is 0 the element is found
-	{
-            *found = true;
-	     return mid;
-	}
+        if(compare < 0) //when compare is less than 1 the top half of the array doens't have the string
+            hi = mid -1; //hi is now mid, begin again
         else if(compare > 0) //when compare is greater than 0 the bottom half of the array doesn't have the string
             lo = mid +1; //lo is now mid
-        else //compare is less than 1 element is in the lower half
+        else 
         {
-            hi = mid -1;
+            //when the string is found mid is the location
+            *found = true;
+            return mid;
         }
     }
     *found = false;
     return(lo);
 }
-/*
- *Function: destroySet
- *
- * Description: destroys the entire set
- *	O(n)
- */
 
 void destroySet(SET *sp)
 {
@@ -79,35 +61,25 @@ void destroySet(SET *sp)
     free(sp->data);
     free(sp);  
 }
-/*
- *Function: findElement
- *
- *Description: uses search to check for the element
- *	O(n)
- */
+
 char *findElement(SET *sp, char *elt)
 {
     assert(sp != NULL);
     int i;
-    bool found = true;
+    bool found;
     i = search(sp, elt, &found);
-    if(found == false)
-        return NULL;
+    if(i == 0)
+        return(-1);
     return(sp->data[i]); //return the index, this is used to delete
 }
-/*
- *Function: addElement
- *
- * Description: adds the elements into the array
- *	O(n)
- */
+
 
 void addElement(SET *sp, char *elt)
 {
     int i;
     assert(sp != NULL);
     assert(elt != NULL);
-    bool found;
+    bool found = false;
     if(sp->count == sp->length) //if the count is the same as the length the array is full
         return;
     int idx = search(sp, elt, &found); //pass to find elements
@@ -115,29 +87,22 @@ void addElement(SET *sp, char *elt)
         return;
     else
     {
-        for(i = sp->count; i > idx; i--){
+        for(i = sp->count; i > idx; i--)
             sp->data[i] = sp->data[i-1]; //moves the elements into the set
-	}
         //copy elements into array
         sp->data[idx] = strdup(elt);
         (sp->count)++;
         return;
     }
 }
-/*
- *Function: removeElement
- *
- * Description: removes an element when it already exists
- * 	O(n)
- *
- */
+
 void removeElement(SET *sp, char *elt)
 {
     assert(sp != NULL);
     assert(elt != NULL);
     int idx;
     int i;
-    bool found = false;
+    bool found;
     idx = search(sp, elt, &found); //pass the element to findElement to check for it
     if(found == false)
         return; //not found
@@ -148,31 +113,17 @@ void removeElement(SET *sp, char *elt)
         return;
     }
     free(sp->data[idx]);
-    for(i = idx+1; i < (sp->count); i++) //shift the element down
-        sp->data[i-1] = sp->data[i];
+    for(i = idx; i < (sp->count) -1; i++) //shift the element down
+        sp->data[i] = sp->data[i+1];
     (sp->count)--;
 }
 
-/*
- *Function: numElements
- *
- *Description: returns the number of elements in the array
- *	O(1)
- *
- */
 
 int numElements(SET *sp)
 {
     return(sp->count);
 }
 
-/*
- *Function: getElements
- *
- *Description: creates a new set of arrays for given words
- *	O(n)
- *
- */
 char **getElements(SET *sp)
 {
     char *elements; //pointer to elemtns
