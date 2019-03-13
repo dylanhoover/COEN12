598ddbec6933f186af5625b69f5f1803760a059c
dylanhoover <dylangranthoover@gmail.com>
2019-03-12 21:26:10 -0700

    working version of app3

--- src/termProject/app3/dataSet.c
+++ src/termProject/app3/dataSet.c
@@ -15,13 +15,14 @@ typedef struct node
 struct set
 {
     NODE *head;
-    NODE *tail;
+   // NODE *tail;
     int count;
 };
 
 
 SET *createSet()
 {
+    printf("creating set\n");
     SET *sp;
     sp = malloc(sizeof(SET));
     assert(sp != NULL);
@@ -30,7 +31,7 @@ SET *createSet()
     assert(sp->head != NULL);
     sp->head->next = sp->head;
     sp->head->prev = sp->head;
-    sp->tail = sp->head;
+  
     return sp;
 }
 
@@ -45,25 +46,27 @@ void destroy(SET *sp)
         free(pDel);
         pDel = pNext;
     } while (pDel != sp->head);
+    printf("Set destroyed, goodbye\n");
     free(sp);
 }
 
 static void *searchA(SET *sp, int age,  bool *found)
 {
     assert(sp != NULL);
-    printf("starting serachA\n");
+   // printf("starting serachA\n");
     NODE *pSearch = sp->head->next;
-    printf("pSearch created\n");
-        do
+    int i;
+    //printf("pSearch created\n");
+        for(i = 0; i < sp->count; i++)
         {
-            pSearch = pSearch->next;
             if(pSearch->age == age)
             {
                 printf("Student with age %d found\n", age);
                 *found = true;
                 return(pSearch);
             }
-        } while (pSearch->next != sp->head);
+            pSearch = pSearch->next;
+        }
         printf("Student with age %d does not exist\n", age);
         *found = false;
         return NULL;
@@ -74,18 +77,19 @@ static void *searchB(SET *sp, int ID, bool *found)
     assert(sp != NULL);
     
     NODE *pSearch = sp->head->next;
-      do
-        {
-            if(pSearch->ID == ID)
+    do
+    {
+        if(pSearch->ID == ID)
             {
-                printf("Student with ID %d found\n", ID);
+                printf("Student with ID: %d found\n", ID);
                 *found = true;
                 return(pSearch);
             }
             pSearch = pSearch->next;
-        } while (pSearch->next != sp->head);
-        printf("Student with ID %d does not exist\n", ID);
-        return NULL;
+
+    } while (pSearch != sp->head);
+    printf("Student with ID %d does not exist\n", ID);
+    *found = false;
 }
 
 void *searchAge(SET *sp, int age)
@@ -122,59 +126,65 @@ void insertElement(SET *sp, int age, int ID)
         new->next = p->next;
         new->prev = p;
         p->next = new;
+        printf("Student inserted\n");
+        sp->count++;
+        return;
     }
     else
     {
         printf("Inserting student aged %d with ID: %d\n", age, ID);
         q = sp->head->next;
+        
         if(sp->count == 0)
         {
             sp->head->next = new;
             new->prev = sp->head;
-            new->next = sp->tail;
-            sp->tail = sp->tail;
+            new->next = sp->head;
+            printf("Student inserted\n");
+            sp->count++;
+            return;
         }
         do
         {
+            if(new->age <= q->age)
+            {
+                new->next = q;
+                new->prev = q->prev;
+                q->prev->next = new;
+                q->prev = new;
+                printf("Student inserted\n");
+                sp->count++;
+                return;
+            }
             q = q->next;
-        } while (new->age > q->age);
-        if(q == sp->head)
-        {
-            new->next = q;
-            new->prev = q->prev;
-            q->prev = new;
-        }
-        else if(q == sp->tail)
-        {
-            new->prev = sp->tail->prev;
-            sp->tail->prev->next = new;
-            new->next = sp->tail->next;
-            sp->tail = new;
-        }
-        else
-        {
-            new->next = q;
-            new->prev = q->prev;
-            new->prev->next = new;
-            q->prev = new;
-        }        
+        } while (q->next != sp->head);
     }
-    printf("Student inserted\n");
-    sp->count++;
-    return;
+   
 }
 
 void deleteElement(SET *sp, int ID)
 {
     assert(sp != NULL);
     bool found;
+    printf("starting delete element\n");
     NODE *pDel = searchB(sp, ID, &found);
-    pDel->prev->next = pDel->next;
-    pDel->next->prev = pDel->prev;
-    if(pDel == sp->tail)
-        sp->tail = pDel->prev;
-    free(pDel);
-
+    if(found == true)
+    {
+        pDel->prev->next = pDel->next;
+        pDel->next->prev = pDel->prev;
+        free(pDel);
+        printf("student deleted\n");
+    }
+    else
+    {
+        printf("no student with that ID\n");
+    }
 }
 
-int maxAgeGap(SET *sp);
+int maxAgeGap(SET *sp)
+{
+    assert(sp != NULL);
+    int minAge = sp->head->next->age;
+    int maxAge = sp->head->prev->age;
+    return(maxAge - minAge);
+}
